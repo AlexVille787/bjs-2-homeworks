6a25c22b6aeb5391b37e9c9b8602b0ca3226ecee
Alex <kulinaleksey@yandex.ru>
2026-01-21 20:13:04 +0300

    zadacha#1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,9 +1,18 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
-}
+  const d = b**2-4 * a * c;
+  if(d<0){
+    return []
+  }
+  if(d === 0){
+    const x = -b/(2*a);
+    return [x]
+  }
+  const x1 = (-b + Math.sqrt(d) )/(2*a);
+  const x2 = (-b - Math.sqrt(d) )/(2*a);
+  return [x1,x2]
+  }
+
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
