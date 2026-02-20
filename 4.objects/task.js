1b95a31a23a5fcf7edef1ee58d8aed8726b09cd5
Alex <kulinaleksey@yandex.ru>
2026-02-20 20:33:04 +0300

    zadanie tri

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,45 @@
 function Student(name, gender, age) {
-  
+ this.name = name;
+ this.gender = gender;
+ this.age = age; 
+ this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if(this.excluded){
+    return
+  }
+    this.marks = [...this.marks,...marks]
 }
 
 Student.prototype.getAverage = function () {
-  
+  if(this.excluded || this.marks.length === 0 ){
+    return 0 
+  }
+    const sum = this.marks.reduce((acc,curr)=>acc+curr,0)
+  let average = sum/this.marks.length
+  return average
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  this.excluded = reason;
+  delete this.subject
+  delete this.marks
+
 }
+let student1 = new Student("Василиса", "женский", 19);
+student1.setSubject("Algebra");
+console.log(student1.getAverage()); // 0
+student1.addMarks(4, 5, 4, 5);
+console.log(student1.getAverage()); // 4.5
+console.log(student1);
+// {age: 19, gender: "женский", marks: [4, 5, 4, 5], name: "Василиса", subject: "Algebra"}
+let student2 = new Student("Артём", "мужской", 25);
+student2.setSubject("Geometry");
+student2.exclude('плохая учёба')
+console.log(student2)
+// {name: "Артём", gender: "мужской", age: 25, excluded: "плохая учёба"}
