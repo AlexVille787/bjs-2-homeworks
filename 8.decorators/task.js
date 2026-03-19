e477ec7a2f8caf2c376ade32e0d7cb26c5dc3132
Alex <kulinaleksey@yandex.ru>
2026-03-19 20:28:34 +0300

    Первая часть

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,20 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+  const cache = [];
+  return function (...args) {
+    let key = md5(args);
+    let cachedData = cache.find((object) => object.hash === key);
+    if (cachedData !== undefined) {
+      return `Из кеша: ${cachedData.result}`;
+    }
+    let funcResult = func(...args);
+    cache.push({ hash: key, result: funcResult });
+    if (cache.length > 5) {
+      cache.shift();
+    }
+    return `Вычисляем: ${funcResult}`;
+  };
 }
 
 //Задача № 2
-function debounceDecoratorNew(func, delay) {
-  
-}
+function debounceDecoratorNew(func, delay) {}
